149825f56bbaaefe0003a900fbe6f04fd8e9683b
Sutan Syahdinullah S <ssyahdinullahs@gmail.com>
2021-06-09 20:33:49 +0700

    Test Update

--- resources/views/content/dashboard.blade.php
+++ resources/views/content/dashboard.blade.php
@@ -5,14 +5,16 @@
         <div class="panel-header bg-primary-gradient">
             <div class="page-inner py-5">
                 <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
-                    <div>
+                    {{-- <div>
                         <h2 class="text-white pb-2 fw-bold">Dashboard</h2>
                         <h5 class="text-white op-7 mb-2">Free Bootstrap 4 Admin Dashboard</h5>
                     </div>
                     <div class="ml-md-auto py-2 py-md-0">
                         <a href="#" class="btn btn-white btn-border btn-round mr-2">Manage</a>
                         <a href="#" class="btn btn-secondary btn-round">Add Customer</a>
-                    </div>
+                    </div> --}}
+
+                    TEST UPDATE
                 </div>
             </div>
         </div>
